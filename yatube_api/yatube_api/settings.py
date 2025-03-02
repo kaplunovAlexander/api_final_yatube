a3891c7fc6a077c23938980a15d8d1a5cce095b9
ashen00ne <kaplunov2304@gmail.com>
2025-03-03 00:02:40 +0300

    PEP8

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -101,7 +101,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATICFILES_DIRS = str(BASE_DIR / 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
